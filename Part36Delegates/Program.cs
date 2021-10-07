6c805f252c6dda0494c50e21da91ccb23bec34ef
priyasonar1207 <83229578+priyasonar1207@users.noreply.github.com>
2021-10-07 13:32:08 +0530

    Add project files.

---
+++ Part36Delegates/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Part36Delegates
+{
+   public delegate void HelloFromDele(string strmassage);
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            HelloFromDele dele = new HelloFromDele(Hello);
+            dele("Hello From Delegate");
+        }
+        public static void Hello(string strmassage)
+        {
+            Console.WriteLine(strmassage);
+        }
+    }
+}
